e1cebe40441a050dcf5f64a14266accf96111714
BennEntterprise <kyle@bennentterprise.com>
2020-11-10 16:43:26 +0000

    Tidy up Server side

--- code/controllers/comments.js
+++ code/controllers/comments.js
@@ -2,15 +2,26 @@ const Comment = require('../models/Comment')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/asyncHandler')
 
+// @desc Get All Comments
+// @route /api/comments
+// @access Public
+exports.getAllComments = asyncHandler(async (req, res, next) => {
+  const comments = await Comment.find()
+  if (!comments) {
+    next(new ErrorResponse(`There was an error accessing the comments.`, 404))
+  }
+  res.status(200).json(comments)
+})
 
 // @desc Get a single comment
 // @route /api/comments/:id
 // @access Public
 exports.getSingleComment = asyncHandler(async (req, res, next) => {
-    const comments = await Comment.findById(req.params.id)
-    res.status(200).json(comments)
-    if (!comments) {
-        next(new ErrorResponse(`Comment not found with id of ${req.params.id}`, 404))
-    }
+  const comment = await Comment.findById(req.params.id)
+  if (!comment) {
+    next(
+      new ErrorResponse(`Comment not found with id of ${req.params.id}`, 404)
+    )
+  }
+  res.status(200).json(comment)
 })
-
--- code/routes/comments.js
+++ code/routes/comments.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 
 const getSingleComment = require('../controllers/comments').getSingleComment
+const getAllComments = require('../controllers/comments').getAllComments
 
-
-router
-    .get('/:id', getSingleComment)
-
+router.get('/', getAllComments).get('/:id', getSingleComment)
 
 module.exports = router
